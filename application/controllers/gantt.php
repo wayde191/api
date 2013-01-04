eba79e778fc5c394a6e5729cb62e6faf44ff251e
wayde191 <hakula567>
2013-01-04 12:57:28 +0800

    add limit
    limit

--- application/controllers/gantt.php
+++ application/controllers/gantt.php
@@ -2,7 +2,7 @@
 
 global $WP_ROOTPATH;
 
-class Suggestions extends CI_Controller {
+class Gantt extends CI_Controller {
 
 	public function index()
 	{
@@ -10,32 +10,23 @@ class Suggestions extends CI_Controller {
 	}
     
   public function getAll() {
-      
-        session_start();
-      $awardsArr = array();
+    session_start();
+    $awardsArr = array();
         
         if (1) {
             $this->load->database();
             
-            $query = 'SELECT * FROM wp_scrum_task'; 
+            $query = 'SELECT * FROM wp_scrum_task limit 11,20'; 
             $query = $this->db->query($query);
             foreach ($query->result() as $row)
             {
-              $award = array('id' => $row->ID, 'text' => $row->name . ' : ' .$row->begin_date);
+              $award = array('id' => $row->id, 'text' => $row->name . ' : ' .$row->begin_date);
               array_push($awardsArr, $award);
             }
 
             echo json_encode(array("status" => 1, "data" => $awardsArr));
-              
-            } else {
-              echo json_encode(array("status" => 0));
-            }
-
-        } else {
-            echo json_encode(array("status" => 0, "errorCode" => -1));
         }
-  }
+    }
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+?>
