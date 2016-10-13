a09777fc90138eb871f471dd84057f2a8585c864
wayde191 <wwsun@thoughtworks.com>
2016-10-13 21:01:16 +0800

    for weixin; Wayde

--- application/controllers/NHWeixin.php
+++ application/controllers/NHWeixin.php
@@ -1,75 +1,81 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    
-    //http://127.0.0.1/api/index.php/nhweixin/
-    
     class NHWeixin extends CI_Controller {
-        
+
         function __construct()
         {
             parent::__construct();
             $this->token = 'ihakulaweixin';
             $this->rootUrl = 'http://www.ihakula.com:9000/no1/index.php?ihakulaweixin';
         }
-        
+
         public function index()
         {
+	        date_default_timezone_set('Asia/Chongqing');
             if (isset($_GET['echostr'])) {
                 $this->valid();
             }else{
                 $this->fetchResponseMsg();
             }
         }
-        
+
         public function fetchResponseMsg(){
             $reqeustXml = $GLOBALS["HTTP_RAW_POST_DATA"];
             $keywork = "ihakula_northern_hemisphere";
-            
+
             $data = array('ihakula_request' => $keywork, 'request_xml' => $reqeustXml);
             $res = $this->send_request($this->rootUrl, $data, 'POST');
-            
+
             $this->logger('Request: ' . $reqeustXml);
             $this->logger('Response: ' . $res);
-            
+
             echo $res;
         }
-        
+
         private function logger($log_content)
         {
             $max_size = 10000;
             $log_filename = "/var/www/php/log.xml";
-            if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
             file_put_contents($log_filename, date('Y-m-d H:i:s')." ".$log_content."\r\n", FILE_APPEND);
         }
-        
+
         public function valid()
         {
             $echoStr = $_GET["echostr"];
             $this->logger($echoStr);
             if($this->checkSignature()){
+		        $this->logger('Done...');
                 echo $echoStr;
                 exit;
             }
         }
-        
+
         private function checkSignature()
         {
             $signature = $_GET["signature"];
             $timestamp = $_GET["timestamp"];
             $nonce = $_GET["nonce"];
-            
+
+	        $this->logger($signature);
+	        $this->logger($timestamp);
+	        $this->logger($nonce);
+
             $token = $this->token;
             $tmpArr = array($token, $timestamp, $nonce);
             sort($tmpArr, SORT_STRING);
             $tmpStr = implode( $tmpArr );
             $tmpStr = sha1( $tmpStr );
-            
+
+	        $this->logger($tmpStr);
+
             if( $tmpStr == $signature ){
-                return true;
+		        $this->logger('yes');
+	   	        return true;
             }else{
+		        $this->logger('no');
                 return false;
             }
         }
-        
+
         public function responseMsg()
         {
             $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
@@ -100,7 +106,7 @@
                 exit;
             }
         }
-        
+
         /**
          * 发送HTTP请求
          *
@@ -127,7 +133,7 @@
                 if ($refererUrl) {
                     curl_setopt($ch, CURLOPT_REFERER, $refererUrl);
                 }
-                
+
                 if(is_string($data)){
                     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                 } else {
@@ -139,7 +145,7 @@
                 } else {
                     $real_url = $url. (strpos($url, '?') === false ? '?' : ''). http_build_query($data);
                 }
-                
+
                 $ch = curl_init($real_url);
                 curl_setopt($ch, CURLOPT_HEADER, 0);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -151,7 +157,7 @@
                 $args = func_get_args();
                 return false;
             }
-            
+
             if($proxy) {
                 curl_setopt($ch, CURLOPT_PROXY, $proxy);
             }
@@ -168,7 +174,7 @@
             curl_close($ch);
             return $ret;
         }
-        
+
     }
-    
+
 ?>
