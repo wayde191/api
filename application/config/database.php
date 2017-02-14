1cd76078ee4936d55ea71fef5c8ff8d2a591a4a3
Wei Sun <wsun@CNwsun.local>
2017-02-14 10:38:56 +0800

    change config; Wayde

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '127.0.0.1';
+$db['default']['hostname'] = '192.168.0.2';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'Wayde191!';
 $db['default']['database'] = 'ihakula';
